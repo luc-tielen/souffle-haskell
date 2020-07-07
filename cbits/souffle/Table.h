717e8c622068a6a319653c05a435d6ca79733fe4
Luc Tielen <luc.tielen@gmail.com>
2020-07-07 17:47:02 +0200

    Minimize set of souffle headers

--- cbits/souffle/Table.h
+++ cbits/souffle/Table.h
@@ -1,6 +1,6 @@
 /*
  * Souffle - A Datalog Compiler
- * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved
+ * Copyright (c) 2020, The Souffle Developers. All rights reserved.
  * Licensed under the Universal Permissive License v 1.0 as shown at:
  * - https://opensource.org/licenses/UPL
  * - <souffle root>/licenses/SOUFFLE-UPL.txt
@@ -8,138 +8,140 @@
 
 /************************************************************************
  *
- * @file Table.h
+ * @file RecordTable.h
  *
- * An implementation of a generic Table storing a position-fixed collection
- * of objects in main memory.
+ * Data container implementing a map between records and their references.
+ * Records are separated by arity, i.e., stored in different RecordMaps.
  *
  ***********************************************************************/
 
 #pragma once
 
-#include <iosfwd>
-#include <iterator>
+#include "CompiledTuple.h"
+#include "RamTypes.h"
+#include <cassert>
+#include <cstddef>
+#include <limits>
+#include <memory>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 namespace souffle {
 
-template <typename T, unsigned blockSize = 4096>
-class Table {
-    struct Block {
-        Block* next;
-        std::size_t used = 0;
-        T data[blockSize];
+/** @brief Bidirectional mappping between records and record references */
+class RecordMap {
+    /** arity of record */
+    const size_t arity;
 
-        Block() : next(nullptr) {}
-
-        bool isFull() const {
-            return used == blockSize;
-        }
-
-        const T& append(const T& element) {
-            const T& res = data[used];
-            data[used] = element;
-            used++;
-            return res;
+    /** hash function for unordered record map */
+    struct RecordHash {
+        std::size_t operator()(std::vector<RamDomain> record) const {
+            std::size_t seed = 0;
+            std::hash<RamDomain> domainHash;
+            for (RamDomain value : record) {
+                seed ^= domainHash(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            }
+            return seed;
         }
     };
 
-    Block* head;
-    Block* tail;
+    /** map from records to references */
+    // TODO (b-scholz): replace vector<RamDomain> with something more memory-frugal
+    std::unordered_map<std::vector<RamDomain>, RamDomain, RecordHash> recordToIndex;
 
-    std::size_t count = 0;
+    /** array of records; index represents record reference */
+    // TODO (b-scholz): replace vector<RamDomain> with something more memory-frugal
+    std::vector<std::vector<RamDomain>> indexToRecord;
 
 public:
-    class iterator : public std::iterator<std::forward_iterator_tag, T> {
-        Block* block;
-        unsigned pos;
+    explicit RecordMap(size_t arity) : arity(arity), indexToRecord(1) {}  // note: index 0 element left free
 
-    public:
-        iterator(Block* block = nullptr, unsigned pos = 0) : block(block), pos(pos) {}
+    /** @brief converts record to a record reference */
+    // TODO (b-scholz): replace vector<RamDomain> with something more memory-frugal
+    RamDomain pack(const std::vector<RamDomain>& vector) {
+        RamDomain index;
+#pragma omp critical(record_pack)
+        {
+            auto pos = recordToIndex.find(vector);
+            if (pos != recordToIndex.end()) {
+                index = pos->second;
+            } else {
+#pragma omp critical(record_unpack)
+                {
+                    indexToRecord.push_back(vector);
+                    index = indexToRecord.size() - 1;
+                    recordToIndex[vector] = index;
 
-        iterator(const iterator&) = default;
-        iterator(iterator&&) = default;
-        iterator& operator=(const iterator&) = default;
-
-        // the equality operator as required by the iterator concept
-        bool operator==(const iterator& other) const {
-            return (block == nullptr && other.block == nullptr) || (block == other.block && pos == other.pos);
-        }
-
-        // the not-equality operator as required by the iterator concept
-        bool operator!=(const iterator& other) const {
-            return !(*this == other);
-        }
-
-        // the deref operator as required by the iterator concept
-        const T& operator*() const {
-            return block->data[pos];
-        }
-
-        // the increment operator as required by the iterator concept
-        iterator& operator++() {
-            // move on in block
-            if (++pos < block->used) {
-                return *this;
+                    // assert that new index is smaller than the range
+                    assert(index != std::numeric_limits<RamDomain>::max());
+                }
             }
-            // or to next block
-            block = block->next;
-            pos = 0;
-            return *this;
         }
-    };
-
-    Table() : head(nullptr), tail(nullptr) {}
-
-    ~Table() {
-        clear();
+        return index;
     }
 
-    bool empty() const {
-        return (!head);
-    }
-
-    std::size_t size() const {
-        return count;
-    }
-
-    const T& insert(const T& element) {
-        // check whether the head is initialized
-        if (!head) {
-            head = new Block();
-            tail = head;
+    /** @brief convert record pointer to a record reference */
+    RamDomain pack(const RamDomain* tuple) {
+        // TODO (b-scholz): data is unnecessarily copied
+        // for a successful lookup. To avoid this, we should
+        // compute a hash of the pointer-array and traverse through
+        // the bucket list of the unordered map finding the record.
+        // Note that in case of non-existence, the record still needs to be
+        // copied for the newly created entry but this will be the less
+        // frequent case.
+        std::vector<RamDomain> tmp(arity);
+        for (size_t i = 0; i < arity; i++) {
+            tmp[i] = tuple[i];
         }
-
-        // check whether tail is full
-        if (tail->isFull()) {
-            tail->next = new Block();
-            tail = tail->next;
-        }
-
-        // increment counter
-        count++;
-
-        // add another element
-        return tail->append(element);
+        return pack(tmp);
     }
 
-    iterator begin() const {
-        return iterator(head);
-    }
-
-    iterator end() const {
-        return iterator();
-    }
-
-    void clear() {
-        while (head != nullptr) {
-            auto cur = head;
-            head = head->next;
-            delete cur;
-        }
-        count = 0;
-        head = nullptr;
-        tail = nullptr;
+    /** @brief convert record reference to a record pointer */
+    const RamDomain* unpack(RamDomain index) const {
+        const RamDomain* res;
+#pragma omp critical(record_unpack)
+        res = indexToRecord[index].data();
+        return res;
     }
 };
 
-}  // end namespace souffle
+class RecordTable {
+public:
+    RecordTable() = default;
+    virtual ~RecordTable() = default;
+
+    /** @brief convert record to record reference */
+    RamDomain pack(RamDomain* tuple, size_t arity) {
+        return lookupArity(arity).pack(tuple);
+    }
+    /** @brief convert record reference to a record */
+    const RamDomain* unpack(RamDomain ref, size_t arity) const {
+        auto iter = maps.find(arity);
+        assert(iter != maps.end() && "Attempting to unpack non-existing record");
+        return (iter->second).unpack(ref);
+    }
+
+private:
+    /** @brief lookup RecordMap for a given arity; if it does not exist, create new RecordMap */
+    RecordMap& lookupArity(size_t arity) {
+        std::unordered_map<size_t, RecordMap>::iterator mapsIterator;
+#pragma omp critical(RecordTableGetForArity)
+        {
+            // This will create a new map if it doesn't exist yet.
+            mapsIterator = maps.emplace(arity, arity).first;
+        }
+        return mapsIterator->second;
+    }
+
+    /** Arity/RecordMap association */
+    std::unordered_map<size_t, RecordMap> maps;
+};
+
+/** @brief helper to convert tuple to record reference for the synthesiser */
+template <std::size_t Arity>
+inline RamDomain pack(RecordTable& recordTab, Tuple<RamDomain, Arity> tuple) {
+    return recordTab.pack(static_cast<RamDomain*>(tuple.data), Arity);
+}
+
+}  // namespace souffle
