1f8970e534b8db03cef7f277062a61717b3db543
Luc Tielen <luc.tielen@gmail.com>
2021-04-25 10:59:47 +0200

    Add benchmarks for repeatedly serialization

--- benchmarks/bench.hs
+++ benchmarks/bench.hs
@@ -12,6 +12,7 @@ import qualified Data.Vector as V
 import GHC.Generics
 import Data.Word
 import Data.Int
+import Control.Monad
 import Control.Monad.IO.Class
 import Control.DeepSeq
 
@@ -42,21 +43,14 @@ instance S.Marshal NumbersFact
 instance S.Marshal StringsFact
 
 
-roundTrip :: (S.ContainsInputFact Benchmarks a, S.ContainsOutputFact Benchmarks a, S.Fact a, S.Submit a)
-          => V.Vector a -> IO (V.Vector a)
-roundTrip vec = S.runSouffle Benchmarks $ \case
-  Nothing -> do
-    liftIO $ print "Failed to load benchmarks!"
-    pure V.empty
-  Just prog -> do
-    S.addFacts prog vec
-    -- No run needed
-    S.getFacts prog
-
-
 -- TODO: uncomment cases with larger numbers (crashes due to large memory allocations?)
 main :: IO ()
 main = defaultMain
+     $ roundTripBenchmarks
+    ++ serializationBenchmarks
+
+roundTripBenchmarks :: [Benchmark]
+roundTripBenchmarks =
   [ bgroup "round trip facts (without strings)"
     [ bench "1"      $ nfIO $ roundTrip $ mkVec 1
     , bench "10"     $ nfIO $ roundTrip $ mkVec 10
@@ -85,3 +79,50 @@ main = defaultMain
   where mkVec count = V.generate count $ \i -> NumbersFact (fromIntegral i) (-42) 3.14
         mkVecStr count = V.generate count $ \i -> StringsFact (fromIntegral i) "abcdef" (-42) 3.14
         mkVecLongStr count = V.generate count $ \i -> StringsFact (fromIntegral i) (T.replicate 10 "abcdef") (-42) 3.14
+
+roundTrip :: (S.ContainsInputFact Benchmarks a, S.ContainsOutputFact Benchmarks a, S.Fact a, S.Submit a)
+          => V.Vector a -> IO (V.Vector a)
+roundTrip vec = S.runSouffle Benchmarks $ \case
+  Nothing -> do
+    liftIO $ print "Failed to load roundtrip benchmarks!"
+    pure V.empty
+  Just prog -> do
+    S.addFacts prog vec
+    -- No run needed
+    S.getFacts prog
+
+
+serializeNumbers :: Int -> IO ()
+serializeNumbers iterationCount = S.runSouffle Benchmarks $ \case
+  Nothing -> liftIO $ print "Failed to load serialize benchmarks!"
+  Just prog ->
+    replicateM_ iterationCount $ S.addFacts prog vec
+    -- No run needed
+  where vec = V.generate 100 $ \i -> NumbersFact (fromIntegral i) (-42) 3.14
+
+serializeWithStrings :: Int -> IO ()
+serializeWithStrings iterationCount = S.runSouffle Benchmarks $ \case
+  Nothing -> liftIO $ print "Failed to load serialize benchmarks!"
+  Just prog ->
+    replicateM_ iterationCount $ S.addFacts prog vec
+    -- No run needed
+  where vec = V.generate 100 $ \i -> StringsFact (fromIntegral i) "abcdef" (-42) 3.14
+
+serializationBenchmarks :: [Benchmark]
+serializationBenchmarks =
+  [ bgroup "serializating facts (without strings)"
+    [ bench "1"      $ nfIO $ serializeNumbers 1
+    , bench "10"     $ nfIO $ serializeNumbers 10
+    , bench "100"    $ nfIO $ serializeNumbers 100
+    , bench "1000"   $ nfIO $ serializeNumbers 1000
+    , bench "10000"  $ nfIO $ serializeNumbers 10000
+    ]
+    -- TODO enable again
+  --, bgroup "serializating facts (with strings)"
+    --[ bench "1"      $ nfIO $ serializeWithStrings 1
+    --, bench "10"     $ nfIO $ serializeWithStrings 10
+    --, bench "100"    $ nfIO $ serializeWithStrings 100
+    --, bench "1000"   $ nfIO $ serializeWithStrings 1000
+    --, bench "10000"  $ nfIO $ serializeWithStrings 10000
+    --]
+  ]
