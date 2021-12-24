99ec743dc9a28c1c3eb2ffaecb22d9003fc60b2d
Providence Salumu <Providence.Salumu@smunix.com>
2021-12-24 15:26:29 -0500

    flake: initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Souffle Datalog bindings for Haskell";
+  inputs = {
+    np.url = "github:nixos/nixpkgs?ref=master";
+    fu.url = "github:numtide/flake-utils?ref=master";
+    hls.url = "github:haskell/haskell-language-server?ref=master";
+  };
+  outputs = { self, np, fu, hls }:
+    with fu.lib;
+    eachSystem [ "x86_64-linux" ] (system:
+      let
+        version = with np.lib;
+          "${substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
+        config = { };
+        overlay = final: _:
+          with final;
+          with haskellPackages.extend (final: _: { }); {
+            souffle-haskell =
+              (callCabal2nix "souffle-haskell" ./. { }).overrideAttrs
+              (o: { version = "${o.version}.${version}"; });
+          };
+        overlays = [ overlay hls.overlay ];
+      in with (import np { inherit system config overlays; }); rec {
+        inherit overlays;
+        packages = flattenTree (recurseIntoAttrs { inherit souffle-haskell; });
+        defaultPackage = packages.souffle-haskell;
+      });
+}
