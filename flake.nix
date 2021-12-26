6b95038a9a5f81ff54002a05f6adfabac57f43c1
Providence Salumu <Providence.Salumu@smunix.com>
2021-12-26 07:38:22 -0500

    fix.macosx: initial commit (WIP)

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
   outputs = { self, np, fu, hls }:
     with fu.lib;
-    eachSystem [ "x86_64-linux" ] (system:
+    eachSystem [ "x86_64-darwin" ] (system:
       let
         version = with np.lib;
           "${substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
@@ -15,8 +15,9 @@
         overlay = final: _:
           with final;
           with haskellPackages.extend (_: _: rec { }); rec {
-            souffle =
-              callPackage (import ./nix/souffle.nix { pkgs = final; }) { };
+            souffle = callPackage (import ./nix/souffle.nix { pkgs = final; }) {
+              inherit (llvmPackages_10) stdenv;
+            };
             souffle-haskell = with haskell.lib;
               (overrideCabal
                 (addBuildTools (callCabal2nix "souffle-haskell" ./. { }) [
