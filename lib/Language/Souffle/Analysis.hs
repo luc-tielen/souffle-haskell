6a556c8b7657aeb2c791cbf0def343e2172b969b
Luc Tielen <luc.tielen@gmail.com>
2022-02-25 08:43:41 +0100

    Add INLINABLE pragmas for Analysis type

--- lib/Language/Souffle/Analysis.hs
+++ lib/Language/Souffle/Analysis.hs
@@ -40,48 +40,62 @@ mkAnalysis :: (a -> m ()) -- ^ Function for finding facts used by the 'Analysis'
            -> m b         -- ^ Function for retrieving the 'Analysis' results from Souffle.
            -> Analysis m a b
 mkAnalysis f r g = Analysis f r (const g)
+{-# INLINABLE mkAnalysis #-}
 
 -- | Converts an 'Analysis' into an effectful function, so it can be executed.
 execAnalysis :: Applicative m => Analysis m a b -> (a -> m b)
 execAnalysis (Analysis f r g) a = f a *> r *> g a
+{-# INLINABLE execAnalysis #-}
 
 instance Functor m => Functor (Analysis m a) where
   fmap func (Analysis f r g) =
     Analysis f r (fmap func <$> g)
+  {-# INLINABLE fmap #-}
 
 instance Functor m => Profunctor (Analysis m) where
   lmap fn (Analysis f r g) =
     Analysis (lmap fn f) r (lmap fn g)
+  {-# INLINABLE lmap #-}
+
   rmap = fmap
+  {-# INLINABLE rmap #-}
 
 instance (Monoid (m ()), Applicative m) => Applicative (Analysis m a) where
   pure a = Analysis mempty mempty (const $ pure a)
+  {-# INLINABLE pure #-}
 
   Analysis f1 r1 g1 <*> Analysis f2 r2 g2 =
     Analysis (f1 <> f2) (r1 <> r2) (\a -> g1 a <*> g2 a)
+  {-# INLINABLE (<*>) #-}
 
 instance (Semigroup (m ()), Semigroup (m b)) => Semigroup (Analysis m a b) where
   Analysis f1 r1 g1 <> Analysis f2 r2 g2 =
     Analysis (f1 <> f2) (r1 <> r2) (g1 <> g2)
+  {-# INLINABLE (<>) #-}
 
 instance (Monoid (m ()), Monoid (m b)) => Monoid (Analysis m a b) where
   mempty = Analysis mempty mempty mempty
+  {-# INLINABLE mempty #-}
 
 instance (Monoid (m ()), Monad m) => Category (Analysis m) where
   id = Analysis mempty mempty pure
+  {-# INLINABLE id #-}
 
   Analysis f1 r1 g1 . Analysis f2 r2 g2 = Analysis f r1 g
     where
       f = execAnalysis (Analysis f2 r2 g2) >=> f1
       -- NOTE: lazyness avoids work here in g2 in cases where "const" is used
       g = g2 >=> g1
+  {-# INLINABLE (.) #-}
 
 instance Functor m => Strong (Analysis m) where
   first' (Analysis f r g) =
     Analysis (f . fst) r $ \(b, d) -> (,d) <$> g b
+  {-# INLINABLE first' #-}
 
   second' (Analysis f r g) =
     Analysis (f . snd) r $ \(d, b) -> (d,) <$> g b
+  {-# INLINABLE second' #-}
 
 instance Applicative m => Choice (Analysis m) where
   left' (Analysis f r g) = Analysis f' r g'
@@ -92,6 +106,7 @@ instance Applicative m => Choice (Analysis m) where
       g' = \case
         Left b -> Left <$> g b
         Right d -> pure $ Right d
+  {-# INLINABLE left' #-}
 
   right' (Analysis f r g) = Analysis f' r g'
     where
@@ -101,27 +116,35 @@ instance Applicative m => Choice (Analysis m) where
       g' = \case
         Left d -> pure $ Left d
         Right b -> Right <$> g b
+  {-# INLINABLE right' #-}
 
 instance (Monad m, Monoid (m ()), Category (Analysis m)) => Arrow (Analysis m) where
   arr f = Analysis mempty mempty (pure . f)
+  {-# INLINABLE arr #-}
 
   first = first'
+  {-# INLINABLE first #-}
 
   second = second'
+  {-# INLINABLE second #-}
 
   Analysis f1 r1 g1 *** Analysis f2 r2 g2 =
     Analysis (\(b, b') -> f1 b *> f2 b') (r1 <> r2) $ \(b, b') -> do
       c <- g1 b
       c' <- g2 b'
       pure (c, c')
+  {-# INLINABLE (***) #-}
 
   Analysis f1 r1 g1 &&& Analysis f2 r2 g2 =
     Analysis (f1 <> f2) (r1 <> r2) $ \b -> (,) <$> g1 b <*> g2 b
+  {-# INLINABLE (&&&) #-}
 
 instance (Monad m, Monoid (m ())) => ArrowChoice (Analysis m) where
   left = left'
+  {-# INLINABLE left #-}
 
   right = right'
+  {-# INLINABLE right #-}
 
   Analysis f1 r1 g1 +++ Analysis f2 r2 g2 = Analysis f' (r1 <> r2) g'
     where
@@ -131,4 +154,5 @@ instance (Monad m, Monoid (m ())) => ArrowChoice (Analysis m) where
       g' = \case
         Left b -> Left <$> g1 b
         Right b' -> Right <$> g2 b'
+  {-# INLINABLE (+++) #-}
 
